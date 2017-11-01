f5280057e1aaaf66da0452229d51899abcc33fe3
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-01 16:57:12 -0400

    chat improved

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -23,30 +23,33 @@ export class ChatPage {
   public simulatedObj;
   public scrollVar;
   public chatObject: {user:any, chatid: number};
+  public isTyping = '';
+  public Rx;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public socket: Socket,
-    public toastCtrl: ToastController
+    public toastCtrl: ToastController,
+    // messageEcho: {message: string}
   ) {
       // Binding page to the passed arguments from Chats page
       this.user = this.navParams.get('user');
-      this.chatid = this.navParams.get('chatid');
+      this.chat = this.navParams.get('chat');
       //listening to changes in messageHistory object  
       this.getHistory().subscribe(messageHistory => {
         this.messages = messageHistory;
       });
 
       this.onIm().subscribe(newMessage=> {
-        console.log('Im receiving IM');
+        console.log('Im receiving IM', newMessage);
         this.messages.push(newMessage);
       })
 
-      // this.onEcho().subscribe(messageEcho => {
-      //   this.errorMessage = messageEcho;
-      //   this.showToast(messageEcho);
-      // });
+      this.onEcho().subscribe(messageEcho => {
+        this.errorMessage = messageEcho;
+        console.log('ECHO', messageEcho)
+      });
   }
 
 
@@ -66,35 +69,76 @@ export class ChatPage {
     let observable = new Observable(observer=> {
       this.socket.on('im', (data)=> {
         observer.next(data);
-        console.log(data);
       });
     });
     return observable;
+  }
+
+  // echo returned observable
+  onEcho() {
+    let observable = new Observable(observer=> {
+      this.socket.on('echo', (data)=> {
+        observer.next(data);
+      });
+    });
+    return observable;
+  }
+
+  onTyping() {
+    let observable = new Observable(observer=> {
+      this.socket.on('typing', (data)=> {
+        observer.next(data);
+      })
+    })
+    return observable;
+  }
+
+  onChange(e) {
+
+    var event = e;
+    console.log(event);
+    // var x = this.Rx.Observable.fromEvent(document,'userInput');
+    // var result = x.throttleTime(2000);
+    // var filtered = e.throttle(2000);
+    // console.log('RESULT IS:', filtered);
+    // Observable.fromEvent(x, 'keyup')
+    //   .throttleTime(5000)
+    //   .subscribe((event) => {
+    //     console.log('event', event);
+    //     // this.onChange(event.value);
+    //     this.socket.emit('typing', this.user, this.chat.members[0]);
+    //     // console.log('CHAT', this.chat);
+    //   })
+
   }
 
 
   
   sendMessage() {
     this.messageObj = {
-      chatid: this.chatid,
+      chatid: this.chat.chatid,
       userid: this.user.id,
       typeId: 3,
-      text: this.message
+      message: this.message,
+      to: this.chat.members[0],
+      sentby: JSON.stringify(this.user),
+      createdOn: Date.now()
     }
-
     //simulated object to display our message instantly
     this.simulatedObj = {
-      chatid: this.chatid,
+      chatid: this.chat.chatid,
       // userid: this.user.id,
-      sentby: JSON.stringify(this.user),
+      sentby: JSON.stringify(fakeObject),
       createdOn: Date.now(),
       typeId: 3,
       message: this.message
     }
     //emitting event with wrapped message object
     this.socket.emit('im', this.messageObj);
+    // console.log(this.messageObj);
     //pushing our simulated object to display sent message
     this.messages.push(this.simulatedObj);
+    // console.log(this.simulatedObj);
     //setting input field to empty string
     this.message = '';
   }
@@ -109,7 +153,7 @@ export class ChatPage {
     this.scrollVar = setTimeout(()=> {
       this.content.scrollToBottom(300);
     }, 500);
-    console.log('CHAT', this.chat)
+    // console.log('CHAT', this.chat)
   }
 
   ionViewWillLeave() {
