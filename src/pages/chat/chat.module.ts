ddbef7b99c2861e88f2cd607f139b76917d4364c
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-08 12:56:28 -0500

    added string pipes

--- src/pages/chat/chat.module.ts
+++ src/pages/chat/chat.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ChatPage } from './chat';
-
+import { NgStringPipesModule } from 'angular-pipes'
 @NgModule({
   declarations: [
     ChatPage,
   ],
   imports: [
     IonicPageModule.forChild(ChatPage),
+    NgStringPipesModule
   ],
 })
 export class ChatPageModule {}
